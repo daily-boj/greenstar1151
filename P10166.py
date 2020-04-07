5a704a728f5e5a01bfc2b4257cab3041ad7667af
Greenstar <greenstar1151@gmail.com>
2020-04-08 02:10:12 +0900

    solved 10166 in naive way

---
+++ P10166.py
@@ -0,0 +1,11 @@
+# 수학 내다버린 풀이
+
+D_1, D_2 = map(int, input().split())
+seatcount = 0
+list_temp = []
+
+for i in range(D_1, D_2 + 1):
+    for j in range(i+1):
+        list_temp.append(j / i)
+
+print(len(list(set(list_temp))) - 1)
