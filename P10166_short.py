022158a5e902b4d7090a38f881f800f96dab0a5c
Greenstar <greenstar1151@gmail.com>
2020-04-08 02:23:05 +0900

    added shorter 10166

--- P10166_short.py
+++ P10166_short.py
@@ -1,6 +1,5 @@
 a,b=map(int,input().split())
 c=[]
-for i in range(a,b+1):
-    for j in range(i+1):
-        c.append(j/i)
+for i in range(a,b+1): 
+ for j in range(i+1):c.append(j/i)
 print(len(list(set(c)))-1)
