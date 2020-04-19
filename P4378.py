360121de52c4d2e3161eccf6531d02f433af2921
greenstar1151 <39073901+greenstar1151@users.noreply.github.com>
2020-04-20 00:51:55 +0900

    asking for help for 4378

---
+++ P4378.py
@@ -0,0 +1,15 @@
+keyboard = ['`','1','2','3','4','5','6','7','8','9','0','-','=','','Q','W','E','R','T','Y','U','I','O','P','[',']','\\','','A','S','D','F','G','H','J','K','L',';',"'",'','Z','X','C','V','B','N','M',',','.','/']
+s_out = ''
+
+s_in = input()
+while s_in != '':
+    for s in s_in:   
+        if s == ' ':
+            s_out += ' '
+        else:
+            i = keyboard.index(s)
+            s_out += keyboard[i - 1]
+    else:
+        print(s_out)
+        s_out = ''
+    s_in = input()
