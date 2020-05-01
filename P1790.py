bbe6d07680d750454eee3d720dcd7177d99edafd
Greenstar <greenstar1151@gmail.com>
2020-05-01 16:16:20 +0900

    sync 1790

--- P1790.py
+++ P1790.py
@@ -18,25 +18,27 @@ N, k = map(int, input().split())
 expN = math.floor(math.log10(N))
 expk = math.floor(math.log10(k))
 numlen_N_prev = numlen_prev(N)
-numlen_knum = numlen_prev(k)
+base10_k_prev = 0
 numlen_k_prev = 0
 
 
 for i in range(9):
     numlen_k_prev += 10**i * 9 * (i+1)
+    base10_k_prev += 1
     if numlen_k_prev >= k:
         numlen_k_prev -= 10**i * 9 * (i+1)
+        base10_k_prev -= 1
         break
     else:
         continue
     
-
-
-number = str((10**expk) + (math.floor((k - numlen_knum - 1) / (expk + 1))))
+# 1234567891011121314151617181920
+number = str(10**base10_k_prev + ((k - numlen_k_prev)//expk))
+#number = str((10**expk) + (math.floor((k - numlen_knum - 1) / (expk + 1))))
 numlen = numlen_N_prev + (N - 10**expN + 1)*(expN + 1)
 
 # debug
-print('numlen : {}, numlen_k_prev : {}, number : {}' .format(numlen, numlen_k_prev, number))
+print('base10_k_prev : {}, numlen : {}, numlen_k_prev : {}, number : {}' .format(base10_k_prev, numlen, numlen_k_prev, number))
 print('{} % {} = {}' .format((k - numlen_k_prev - 1), ((math.floor(math.log10(int(number)))) + 1), ((k - numlen_k_prev - 1) % ((math.floor(math.log10(int(number)))) + 1))))
 
 if k > numlen:
